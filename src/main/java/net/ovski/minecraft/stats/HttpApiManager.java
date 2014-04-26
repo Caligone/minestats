3c07dc578014545dff1b9622e9fbb1a8e3da3b24
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2014-04-26 15:20:44 +0200

    Implement api calls

--- src/main/java/net/ovski/minecraft/stats/HttpApiManager.java
+++ src/main/java/net/ovski/minecraft/stats/HttpApiManager.java
@@ -21,17 +21,35 @@ public class HttpApiManager
     /**
      * getPlayerStats method get the statistics of a player
      * This method is used in case a player request another player stats, even though this one is not currently connected
-     * 
+     * /api/player?key=:key&pseudo=:pseudo
+     *
      * @return playerStats : A PlayerStats object that contains the playerStats of a player
      * @param pseudo : the pseudo of the player
      */
     public static PlayerStats getPlayerStats(String pseudo)
     {
-        return null;
+	String key = StatsPlugin.key;
+	String[][] params = new String [][] {
+	    {"key", key},
+	    {"pseudo", pseudo}
+	};
+	String url = HttpTools.createApiUrl("/api/player", params);
+	JSONObject json = HttpTools.sendHttpRequest(url);
+	if (Integer.valueOf(json.get("status").toString()) != 0) {
+	    String message = (String)json.get("message");
+	    Bukkit.getPluginManager().getPlugin("MineStats").getLogger()
+	        .warning("The server sent back an error : "+message);
+
+	    return null;
+    	} else {
+    	    String serverId = JsonTools.getServerId(json, key);
+    	    return JsonTools.getPlayerStats(json, serverId);
+    	}
     }
 
     /**
      * insertKill method insert a new kill in database
+     * /api/playerkilled?key=:key&killer=:killer&killed=:killed&weapon=:weapon&date=:date
      * 
      * @param killerPseudo : Contains the pseudo of the killer
      * @param killedPseudo : Contains the pseudo of the killed player
@@ -40,17 +58,50 @@ public class HttpApiManager
      */
     public static void addKill(String killerPseudo, String killedPseudo, String weaponId, String date)
     {
-        
+	String key = StatsPlugin.key;
+	String[][] params = new String [][] {
+	    {"key", key},
+	    {"killer", killerPseudo},
+	    {"killed", killedPseudo},
+	    {"weapon", weaponId},
+	    {"date", date}
+	};
+	String url = HttpTools.createApiUrl("/api/playerkilled", params);
+	JSONObject json = HttpTools.sendHttpRequest(url);
+	if (Integer.valueOf(json.get("status").toString()) != 0) {
+	    String message = (String)json.get("message");
+	    Bukkit.getPluginManager().getPlugin("MineStats").getLogger()
+	        .warning("The server sent back an error : "+message);
+    	}
     }
 
     /**
      * updateStats method update the statistics of a player
+     * /api/updatePlayer?key=:key&pseudo=:pseudo
+     *  &verbosity=:verbosity&blocksBroken=:blocksBroken
+     *  &blocksPlaced=:blocksPlaced&stupidDeaths=:stupidDeaths&level=:level
      * 
      * @param playerStats : A PlayerStats object that contains the playerStats of a player
      */
     public static void updatePlayerStats(PlayerStats playerStats)
     {
-        
+	String key = StatsPlugin.key;
+	String[][] params = new String [][] {
+	    {"key", key},
+	    {"pseudo", playerStats.getPseudo()},
+	    {"verbosity", String.valueOf(playerStats.getVerbosity())},
+	    {"blocksBroken", String.valueOf(playerStats.getBlocksBroken())},
+	    {"blocksPlaced", String.valueOf(playerStats.getBlocksPlaced())},
+	    {"stupidDeaths", String.valueOf(playerStats.getStupidDeaths())},
+	    {"level", "0"}
+	};
+	String url = HttpTools.createApiUrl("/api/updatePlayer", params);
+	JSONObject json = HttpTools.sendHttpRequest(url);
+	if (Integer.valueOf(json.get("status").toString()) != 0) {
+	    String message = (String)json.get("message");
+	    Bukkit.getPluginManager().getPlugin("MineStats").getLogger()
+	        .warning("The server sent back an error : "+message);
+    	}
     }
 
     /**
@@ -59,8 +110,6 @@ public class HttpApiManager
      * 
      * @param pseudo : the pseudo of the player
      * @return PlayerStats : a playerStats object;
-     * @throws KeyNotSetException 
-     * @throws ServerErrorException 
      */
     public static PlayerStats playerConnect(String pseudo)
     {
@@ -116,7 +165,6 @@ public class HttpApiManager
 		plugin.getLogger().warning("The server sent back an error : "+message);
 	    }
     	}
-    	// TODO TEST API/PLUGIN VERSION
     }
 
     /**
